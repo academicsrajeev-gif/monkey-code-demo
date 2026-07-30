2dcfc991c6a5b31244e86ffef87249c84841dc72
academicsrajeev-gif <academics.rajeev@gmail.com>
2026-07-31 03:20:30 +0530

    Create ParentPortal.jsx

---
+++ ParentPortal.jsx
@@ -0,0 +1,8 @@
+export default function ParentPortal() {
+  return (
+    <div className="max-w-4xl mx-auto py-12 px-4">
+      <h1 className="text-3xl font-bold mb-4">Parent Portal</h1>
+      <p>Login to access your child's information.</p>
+    </div>
+  )
+}

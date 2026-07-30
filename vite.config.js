22d80667f56f5d6c815b1210757917f15721de38
academicsrajeev-gif <academics.rajeev@gmail.com>
2026-07-31 04:52:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Don Bosco Public School Hathaura',
-        short_name: 'Don Bosco',
-        theme_color: '#0066CC',
-      }
-    })
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
   }

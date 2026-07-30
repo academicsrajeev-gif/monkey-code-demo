304c5fe45285370c35d3b5c63f9e97629d8ae717
academicsrajeev-gif <academics.rajeev@gmail.com>
2026-07-31 03:16:55 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-4xl font-bold mb-4">Don Bosco Public School Hathaura</h1>
+      <p className="text-lg text-gray-600">Quality Education & Values, Play to Class VI</p>
+    </div>
+  )
+}

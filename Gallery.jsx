561e9f16b06ebe7053205797aeb39d4cdc315088
academicsrajeev-gif <academics.rajeev@gmail.com>
2026-07-31 03:18:23 +0530

    Create Gallery.jsx

---
+++ Gallery.jsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <div className="max-w-4xl mx-auto py-12 px-4">
+      <h1 className="text-3xl font-bold mb-4">Gallery</h1>
+      <p>Photos and videos of school events.</p>
+    </div>
+  )
+}
